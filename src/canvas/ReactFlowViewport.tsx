ba7eced690eed858e71c325debf6b29f859ae73a
alexevermeulen <ahc.vermeulen@gmail.com>
2026-08-06 21:09:06 +0200

    Add ReactFlowViewport component

---
+++ src/canvas/ReactFlowViewport.tsx
@@ -0,0 +1,15 @@
+import {
+  Background,
+  Controls,
+  MiniMap,
+} from '@xyflow/react'
+
+export function ReactFlowViewport() {
+  return (
+    <>
+      <Background gap={20} size={1} />
+      <Controls />
+      <MiniMap pannable zoomable />
+    </>
+  )
+}
